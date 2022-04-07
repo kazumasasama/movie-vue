6c6fa098aba51c7f5c874cff4f99cb7bfc8dc820
Kaz <takaik@yahoo.com>
2022-04-07 19:28:40 -0400

    improve movies view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,17 @@
 <template>
   <div class="home">
+    <h1>Movies</h1>
     <div v-for="error in errors" :key="error">
       <p>{{ error }}</p>
     </div>
-    <div>
-      <h4>{{ movies }}</h4>
+    <div v-for="movie in movies" :key="movie.id">
+      <h4>{{ movie.title }}</h4>
+      <p>{{ movie.year }}</p>
+      <p>{{ movie.director }}</p>
+      <p>{{ movie.plot }}</p>
+      <img class="movie-img" :src="movie.image" :alt="movie.title" />
+      <hr />
+      <hr />
     </div>
   </div>
 </template>
@@ -37,4 +44,7 @@ export default {
 </script>
 
 <style>
+.movie-img {
+  max-height: 250px;
+}
 </style>
